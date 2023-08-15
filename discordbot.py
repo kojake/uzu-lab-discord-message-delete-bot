32af48d1c034a8394c2ecf9bce4f57163e4b7bd5
kaito <ahonamame@gmail.com>
2023-08-15 14:55:33 +0900

    update

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 import discord
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = ''
+TOKEN = 'MTE0MDg3NjAyMDgwNzg5NzE5MA.GZIMI_.CnNnzaq8N7sLaSIZhcUdxL08JR2Xz3KzUBHSt4'
 
 # 必要な Intents を設定
 intents = discord.Intents.default()
@@ -14,10 +14,14 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+    print('login success')
 
 #不適切メッセージリスト
-Inappropriate_message = ["うんこ"]
+Inappropriate_message = [
+    "うんこ",
+    "ちんこ",
+    "",
+]
 
 # メッセージ受信時に動作する処理
 @client.event
@@ -25,10 +29,12 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content in Inappropriate_message:
-        await message.delete()
-        await message.channel.send("不適切なメッセージが検知されました")
+    #不適切な言葉が入っていないかをチェック
+    content = message.content.lower()  # メッセージを小文字に変換して検査
+    for word in Inappropriate_message:
+        if word in content:
+            await message.delete()  # 不適切な言葉が含まれている場合、メッセージを削除
+            await message.channel.send(f"{message.author.mention} 不適切な言葉が含まれていたため、メッセージを削除しました。")
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
